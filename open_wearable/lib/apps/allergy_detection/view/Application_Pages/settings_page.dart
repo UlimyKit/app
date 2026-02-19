4b11e166563e19be8550822a326b38f61e9c87eb
Cedric Liebhold <ulimy@student.kit.edu>
2026-02-19 12:21:39 +0100

    changed logoutbutton settignspage

--- open_wearable/lib/apps/allergy_detection/view/Application_Pages/settings_page.dart
+++ open_wearable/lib/apps/allergy_detection/view/Application_Pages/settings_page.dart
@@ -77,7 +77,7 @@ class _SettingsPageState extends State<SettingsPage> {
         Container(
           margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           decoration: BoxDecoration(
-            color: Colors.red.withValues(alpha: 20),
+            color: Colors.red.withAlpha(30),
             borderRadius: BorderRadius.circular(12),
           ),
           child: ListTile(
